c77fb5899c069d6b0791dedb63fae5c5cc3b061d
or-feus <barca105@nate.com>
2024-05-17 09:57:21 +0900

    feat: Add keymap of nvimtree

--- nvim/lua/plugins/nvim_tree.lua
+++ nvim/lua/plugins/nvim_tree.lua
@@ -1,27 +1,28 @@
 local setup, nvimtree = pcall(require, "nvim-tree")
 if not setup then
-    return
+	return
 end
 
 vim.g.loaded = 1
 nvimtree.setup({
-    respect_buf_cwd = true,
-    disable_netrw = true,
-    hijack_netrw = true,
+	respect_buf_cwd = true,
+	disable_netrw = true,
+	hijack_netrw = true,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "NvimTree" },
-    callback = function(args)
-        vim.api.nvim_create_autocmd("VimLeavePre", {
-            callback = function()
-                vim.api.nvim_buf_delete(args.buf, { force = true })
-                return true
-            end,
-        })
-    end,
+	pattern = { "NvimTree" },
+	callback = function(args)
+		vim.api.nvim_create_autocmd("VimLeavePre", {
+			callback = function()
+				vim.api.nvim_buf_delete(args.buf, { force = true })
+				return true
+			end,
+		})
+	end,
 })
 
 vim.keymap.set("n", "<Space><Space>", ":NvimTreeFocus<CR>", { silent = true, noremap = true })
 vim.keymap.set("n", "<Space>q", ":NvimTreeClose<CR>", { silent = true, noremap = true })
 vim.keymap.set("n", "<Space>s", ":NvimTreeToggle<CR>", { silent = true, noremap = true })
+vim.keymap.set("n", "<Space>c", ":NvimTreeCollapse<CR>", { silent = true, noremap = true })
