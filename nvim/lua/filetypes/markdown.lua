930d135f7130193800a0a5cfe8ee93d17b808a0e
calmhlynn <barca105@nate.com>
2024-09-20 22:05:33 +0900

    markdown: set render-markdown.nvim

--- nvim/lua/filetypes/markdown.lua
+++ nvim/lua/filetypes/markdown.lua
@@ -1,11 +1,21 @@
 vim.api.nvim_create_autocmd({ "FileType", "BufEnter" }, {
-    group = vim.api.nvim_create_augroup("markdown_sw_is_2", { clear = true }),
-    pattern = "markdown",
-    command = "setlocal shiftwidth=2",
+	group = vim.api.nvim_create_augroup("markdown_sw_is_2", { clear = true }),
+	pattern = "markdown",
+	command = "setlocal shiftwidth=2",
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-    group = vim.api.nvim_create_augroup("markdown_wrap", { clear = true }),
-    pattern = "markdown",
-    command = "set wrap | set textwidth=80",
+	group = vim.api.nvim_create_augroup("markdown_wrap", { clear = true }),
+	pattern = "markdown",
+	command = "set wrap | set textwidth=80",
+})
+
+require("render-markdown").setup({
+	file_types = { "markdown" },
+	render_modes = {
+		"n",
+		"v",
+		"i",
+		"c",
+	},
 })
