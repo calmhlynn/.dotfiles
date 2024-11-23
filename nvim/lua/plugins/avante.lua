7153c230adb7db8f9e49f781b1f5a551b752603a
calmhlynn <barca105@nate.com>
2024-11-23 21:25:24 +0900

    Add avante.nvim

---
+++ nvim/lua/plugins/avante.lua
@@ -0,0 +1,7 @@
+require("avante").setup({
+
+	provider = "openai",
+	openai = {
+		api_key_name = "cmd:security find-generic-password -s OPENAI_KEY -a calmhlynn -w",
+	},
+})
--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,6 +1,6 @@
 require("render-markdown").setup({
-	enabled = false,
-	file_types = { "markdown" },
+	-- enabled = false,
+	file_types = { "markdown", "Avante" },
 	render_modes = { "n", "v", "i", "c" },
 
 	vim.keymap.set("n", "<leader>m", ":RenderMarkdown toggle<CR>", { silent = true }),
