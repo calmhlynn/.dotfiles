6b244c059383ca99a0499a56058693d67ff82a13
Seonkyo Jung <barca105@naver.com>
2025-02-08 12:53:28 +0900

    feat(toggleterm): add keymap

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -3,14 +3,14 @@ require("toggleterm").setup({
 	auto_scroll = false,
 	persist_size = false,
 	persist_mode = false,
-	direction = "float",
+	-- direction = "float",
 })
 
 vim.api.nvim_create_autocmd(
 	"TermEnter",
 	{ pattern = "term://*toggleterm#*", command = [[tnoremap <silent><Esc> <Cmd>exe v:count1 . "ToggleTerm"<CR>]] }
 )
--- vim.keymap.set("n", "t", [[<Cmd>TroubleClose<CR><Cmd>exe v:count1 . "ToggleTerm"<CR>]], { silent = true })
+vim.keymap.set("t", "<C-q>", [[<C-\><C-n>]], { silent = true })
 vim.keymap.set("n", "t", ":ToggleTerm<CR>", { silent = true })
 
 vim.api.nvim_create_autocmd("VimLeavePre", {
