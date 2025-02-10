d2b8f7e1aee7fbdcdac992aa9fe7247508c0dbb3
Seonkyo Jung <barca105@naver.com>
2025-02-10 13:43:52 +0900

    fix(nvim): remove unused options

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,8 @@
 -- Setup Lazy plugin manager
-
 require("plugins.plugin_setup")
 
 -- Settings
-
+vim.g.mapleader = ";"
 vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.tabstop = 4
@@ -17,25 +16,6 @@ vim.opt.ff = "unix"
 vim.opt.scrolloff = 4
 vim.opt.encoding = "utf-8"
 vim.opt.hidden = true
-vim.opt.updatetime = 300
-vim.g.loaded_perl_provider = 0
-vim.wo.signcolumn = "yes"
-vim.opt.clipboard = "unnamedplus"
-vim.cmd([[language en_US]])
-
--- (Global) Key mappings
-
-vim.g.mapleader = ";"
-vim.cmd([[
-nnoremap j gj
-nnoremap gj j
-nnoremap k gk
-nnoremap gk k
-]])
--- vim.keymap.set("i", ";;", "<Esc>", { silent = true, noremap = true})
-
--- Workaround for notify.nvim
--- https://github.com/rcarriga/nvim-notify/issues/63
 vim.opt.updatetime = 500
 vim.opt.redrawtime = 1500
 vim.opt.lazyredraw = true
@@ -43,9 +23,12 @@ vim.opt.timeout = true
 vim.opt.ttimeout = true
 vim.opt.timeoutlen = 500
 vim.opt.ttimeoutlen = 10
+vim.g.loaded_perl_provider = 0
+vim.wo.signcolumn = "yes"
+vim.opt.clipboard = "unnamedplus"
+vim.cmd([[language en_US]])
 
 -- Plugins
-
 require("plugins.auto_session")
 require("plugins.telescope")
 require("plugins.nvim_tree")
@@ -66,20 +49,19 @@ require("plugins.comment")
 require("plugins.markdown")
 require("plugins.util")
 
+-- LSPs
 require("plugins.lsp.lsp")
 require("plugins.lsp.yaml")
--- require("plugins.lsp.rust")
--- require("plugins.lsp.zig")
--- require("plugins.lsp.typescript")
 require("plugins.lsp.lua")
 require("plugins.lsp.python")
-require("plugins.lsp.cpp")
-require("plugins.lsp.terraform")
+-- require("plugins.lsp.zig")
+-- require("plugins.lsp.typescript")
+-- require("plugins.lsp.cpp")
+-- require("plugins.lsp.terraform")
 -- require("plugins.lsp.scala")
 -- require("plugins.lsp.go")
 
 -- Filetypes
-
 require("filetypes.markdown")
 -- require("filetypes.javascript")
 -- require("filetypes.graphql")
