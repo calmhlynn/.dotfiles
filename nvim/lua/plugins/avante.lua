a3ae02d86cb3c55f8859a221ef87e52f29f0bd7f
calmhlynn <barca105@naver.com>
2025-06-05 14:29:25 +0900

    feat(avante.nvim): mode replaced to legacy

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -1,9 +1,17 @@
 require("avante").setup({
 
-	provider = "claude",
-	claude = {
-        model = "claude-sonnet-4-20250514",
-		api_key_name = "cmd:pass show api_tokens/claude",
-        disable_tools = true,
-	},
+    provider = "claude",
+    mode = "legacy", -- "agentic" is too much and expensive..
+	providers = {
+        claude = {
+            model = "claude-sonnet-4-20250514",
+            api_key_name = "cmd:pass show api_tokens/claude",
+            disable_tools = true,
+        },
+        openai = {
+            model = "o4-mini-2025-04-16",
+            api_key_name = "cmd:pass show api_tokens/openai",
+            -- disable_tools = true,
+        }
+    }
 })
