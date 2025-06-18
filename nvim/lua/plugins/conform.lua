52835dd6f843580dc1fecc4518b92be35b7871cc
calmhlynn <barca105@naver.com>
2025-06-18 09:18:45 +0900

    feat(nvim): enable clang-format in conform.nvim

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,9 +1,15 @@
 require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
+		c = { "clang-format" },
+	},
+	formatters = {
+		["clang-format"] = {
+			args = { "--style='{ColumnLimit: 150}'" },
+		},
 	},
 	format_on_save = {
 		timeout_ms = 500,
-		lsp_fallback = true,
+		lsp_format = "fallback",
 	},
 })
