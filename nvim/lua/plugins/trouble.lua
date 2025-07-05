307546af7168ec43deeb3d991dae93056002d07f
calmhlynn <barca105@naver.com>
2025-07-06 01:33:42 +0900

    fix(nvim): delete deprecated files

--- nvim/lua/plugins/trouble.lua
+++
@@ -1,6 +0,0 @@
-require("trouble").setup({ auto_preview = false })
-
-vim.keymap.set("n", "<leader>tt", "<cmd>TroubleToggle<CR>", { silent = true, noremap = true })
-
--- Trouble seems to have trouble with auto-session, so close when VimLeavePre
-vim.api.nvim_create_autocmd("VimLeavePre", { command = [[TroubleClose]] })
