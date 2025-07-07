02e1acd8083b7634cb109d6a6246481f12c93111
calmhlynn <barca105@naver.com>
2025-07-08 08:51:55 +0900

    feat(nvim): add keymap for nvim_cmp

--- nvim/lua/plugins/nvim_cmp.lua
+++ nvim/lua/plugins/nvim_cmp.lua
@@ -32,14 +32,21 @@ return {
 				["<C-k>"] = require("cmp").mapping.complete(),
 				["<C-e>"] = require("cmp").mapping.abort(),
 				["<C-Space>"] = require("cmp").mapping.confirm({ select = true }),
-			}),
-			sources = require("cmp").config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "path" },
-				{ name = "nvim_lsp_signature_help" },
-				{ name = "crates" },
-				{ name = "buffer" },
+				["<Tab>"] = require("cmp").mapping(function(fallback)
+					if require("luasnip").locally_jumpable(1) then
+						require("luasnip").jump(1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<S-Tab>"] = require("cmp").mapping(function(fallback)
+					if require("luasnip").locally_jumpable(-1) then
+						require("luasnip").jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 			}),
 		},
 	},
