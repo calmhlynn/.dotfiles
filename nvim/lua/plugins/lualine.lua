9e0ef41c1d9aae50eec8eab6d39e665842794bc7
calmhlynn <barca105@naver.com>
2025-07-11 14:36:27 +0900

    fix(nvim): fix problems that didn't run

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -49,16 +49,20 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = "nvim-tree/nvim-web-devicons",
 
-	options = {
-		theme = kanagawa_dragon,
-	},
-	sections = {
-		lualine_a = {
-			{
-				"filename",
-				file_status = true, -- displays file status (readonly status, modified status)
-				path = 2, -- 0 = just filename, 1 = relative path, 2 = absolute path
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = kanagawa_dragon,
 			},
-		},
-	},
+			sections = {
+				lualine_a = {
+					{
+						"filename",
+						file_status = true, -- displays file status (readonly status, modified status)
+						path = 2, -- 0 = just filename, 1 = relative path, 2 = absolute path
+					},
+				},
+			},
+		})
+	end,
 }
