b28d7d2a5803a60e3374956ed51e65a134893a84
calmhlynn <barca105@naver.com>
2025-07-11 13:22:53 +0900

    feat(nvim): add highlights for cursorline

--- nvim/lua/plugins/kanagawa.lua
+++ nvim/lua/plugins/kanagawa.lua
@@ -10,9 +10,9 @@ return {
 				return {
 					Normal = { fg = colors.palette.dragonWhite, bg = colors.palette.dragonBlack3 },
 					NormalFloat = { fg = colors.palette.fujiWhite, bg = colors.palette.dragonBlack3 },
-					-- FloatBorder = { fg = colors.palette.fujiWhite, bg = colors.palette.dragonBlack3 },
-
-					-- AvanteSidebarWinSeparator = { fg = colors.palette.fujiWhite, bg = colors.palette.dragonBlack3 },
+					FloatBorder = { fg = colors.palette.fujiWhite, bg = colors.palette.dragonBlack3 },
+					CursorLine = { bg = colors.palette.dragonBlack4 },
+					CursorLineNr = { fg = colors.palette.fujiWhite },
 				}
 			end,
 		})
