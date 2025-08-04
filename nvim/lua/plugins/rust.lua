0a58a3eb950324fad85b369fe6bad91895e27e85
calmhlynn <barca105@naver.com>
2025-08-04 13:59:41 +0900

    fix(nvim): fix config for crates.nvim

--- nvim/lua/plugins/rust.lua
+++ nvim/lua/plugins/rust.lua
@@ -8,22 +8,24 @@ return {
 		"saecki/crates.nvim",
 		tag = "stable",
 
-		lsp = {
-			enabled = true,
-			on_attach = LSP_ON_ATTACH,
-			actions = true,
-			completion = true,
-			hover = true,
-		},
-
-		completion = {
-			crates = {
-				enabled = true,
-				max_results = 8,
-				min_chars = 3,
-			},
-		},
 		config = function()
+			require("crates").setup({
+				lsp = {
+					enabled = true,
+					on_attach = LSP_ON_ATTACH,
+					actions = true,
+					completion = true,
+					hover = true,
+				},
+
+				completion = {
+					crates = {
+						enabled = true,
+						max_results = 8,
+						min_chars = 3,
+					},
+				},
+			})
 			vim.keymap.set(
 				"n",
 				"<Leader>cf",
