b5acfac2c1dad7d3951382dff151b172869556bc
calmhlynn <barca105@naver.com>
2025-08-06 15:54:13 +0900

    feat(conform.nvim): add linter in python

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,20 +1,20 @@
 return {
-		"stevearc/conform.nvim",
-		opts = {
+	"stevearc/conform.nvim",
+	opts = {
 
-			formatters_by_ft = {
-				lua = { "stylua" },
-				c = { "clang-format" },
-			},
-			formatters = {
-				["clang-format"] = {
-					args = { "--style={ColumnLimit: 150}" },
-				},
-			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_format = "fallback",
+		formatters_by_ft = {
+			lua = { "stylua" },
+			c = { "clang-format" },
+			python = { "black" },
+		},
+		formatters = {
+			["clang-format"] = {
+				args = { "--style={ColumnLimit: 150}" },
 			},
 		},
-	}
-
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+	},
+}
