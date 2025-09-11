5da9834d19a4057f7f4eba8287908b855244cee2
calmhlynn <barca105@naver.com>
2025-09-11 13:32:54 +0900

    feat(nvim): add config for ts

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -5,11 +5,22 @@ return {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			c = { "clang-format" },
+			cuda = { "clang-format" },
 			python = { "black" },
+			typescript = { "prettier" },
+			typescriptreact = { "prettier" },
+			javascript = { "prettier" },
+			javascriptreact = { "prettier" },
+			json = { "prettier" },
+			jsonc = { "prettier" },
+			html = { "prettier" },
+			css = { "prettier" },
+			scss = { "prettier" },
+			["*"] = { "trim_whitespace" },
 		},
 		formatters = {
 			["clang-format"] = {
-				args = { "--style={ColumnLimit: 150}" },
+				args = { "--style={BasedOnStyle: 'LLVM', IndentWidth: 4, ColumnLimit: 140}" },
 			},
 		},
 		format_on_save = {
