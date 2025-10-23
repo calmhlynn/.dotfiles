42228c24533fb33a8b0198606e246cb479327220
calmhlynn <barca105@naver.com>
2025-10-23 09:23:56 +0900

    nvim: add rustfmt in conform.nvim

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,19 +4,10 @@ return {
 
 		formatters_by_ft = {
 			lua = { "stylua" },
+			rust = { "rustfmt" },
 			c = { "clang-format" },
 			cuda = { "clang-format" },
 			python = { "black" },
-			typescript = { "prettier" },
-			typescriptreact = { "prettier" },
-			javascript = { "prettier" },
-			javascriptreact = { "prettier" },
-			json = { "prettier" },
-			jsonc = { "prettier" },
-			html = { "prettier" },
-			css = { "prettier" },
-			scss = { "prettier" },
-			["*"] = { "trim_whitespace" },
 		},
 		formatters = {
 			["clang-format"] = {
