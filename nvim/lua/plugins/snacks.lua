fbc105fcc6563229098a45af59525c3b231d2121
calmhlynn <barca105@naver.com>
2025-11-07 11:04:25 +0900

    fix: remove invalid code

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -41,23 +41,6 @@ return {
 		},
 		picker = {
 			enabled = true,
-			layout = {
-				preset = "default",
-				layout = {
-					box = "horizontal",
-					width = 0.8,
-					min_width = 120,
-					height = 0.8,
-					{
-						box = "vertical",
-						border = true,
-						title = "{title} {live} {flags}",
-						{ win = "input", height = 1, border = "bottom" },
-						{ win = "list", border = "none" },
-					},
-					{ win = "preview", title = "{preview}", border = true, width = 0.65 },
-				},
-			},
 			previewers = {
 				diff = {
 					builtin = false,
