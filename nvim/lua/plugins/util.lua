53ef4134f52d2d67226deca42499ce87dce4d4c3
calmhlynn <barca105@naver.com>
2026-03-19 05:35:45 +0000

    feat(nvim,tmux): set vim-tmux-navigator

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -50,4 +50,19 @@ return {
 	{ "NvChad/nvim-colorizer.lua", opts = {
 		filetypes = { "*" },
 	} },
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+		},
+		keys = {
+			{ "<C-w><Left>", "<cmd>TmuxNavigateLeft<cr>" },
+			{ "<C-w><Down>", "<cmd>TmuxNavigateDown<cr>" },
+			{ "<C-w><Up>", "<cmd>TmuxNavigateUp<cr>" },
+			{ "<C-w><Right>", "<cmd>TmuxNavigateRight<cr>" },
+		},
+	},
 }
