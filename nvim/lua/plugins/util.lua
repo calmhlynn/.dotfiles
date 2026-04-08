082017033774d4d9d9ca0aa6e9116af564d32e8f
calmhlynn <barca105@naver.com>
2026-04-08 01:52:52 +0000

    fix(nvim): add navigator no mappings

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -4,6 +4,9 @@ return {
 	} },
 	{
 		"christoomey/vim-tmux-navigator",
+		init = function()
+			vim.g.tmux_navigator_no_mappings = 1
+		end,
 		cmd = {
 			"TmuxNavigateLeft",
 			"TmuxNavigateDown",
